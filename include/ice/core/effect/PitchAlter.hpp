872c7b80a04a08b363d758dc63bfc11ebfd46e9d
xiangCaO233 <odokoei@outlook.com>
2025-08-01 15:12:23 +0800

    添加变调器接口

--- include/ice/core/effect/PitchAlter.hpp
+++ include/ice/core/effect/PitchAlter.hpp
@@ -2,6 +2,7 @@
 #define ICE_PITCHALTER_HPP
 
 #include <atomic>
+#include <cmath>
 #include <ice/core/effect/rubberband/RStretcher.hpp>
 
 #include "ice/core/effect/IEffectNode.hpp"
@@ -10,13 +11,29 @@ namespace ice {
 class PitchAlter : public IEffectNode {
    public:
     /**
+     * @brief 设置音高变化,直接设置倍率
+     * @param scale 音高倍率
+     */
+    inline void set_pitch_scale(double scale) {
+        pitch_scale.store(scale);
+        pitch_semitones.store(12.0 * std::log2(scale));
+    }
+
+    /**
      * @brief 设置音高变化,以半音（semitones）为单位
      * e.g., +12.0f 是升高一个八度, -12.0f 是降低一个八度
      * @param semitones 音高变化
      */
     inline void set_pitch_shift(double semitones) {
         pitch_semitones.store(semitones);
+        pitch_scale.store(std::pow(2.0, pitch_semitones.load() / 12.0));
     }
+
+    // 获取音高变化值
+    inline double semitones() const { return pitch_semitones.load(); }
+
+    // 获取音高倍率变化值
+    inline double scale() const { return pitch_scale.load(); }
 
    protected:
     // 应用变调器
@@ -25,6 +42,7 @@ class PitchAlter : public IEffectNode {
    private:
     // 音高变化
     std::atomic<double> pitch_semitones;
+    std::atomic<double> pitch_scale;
     // 拉伸器
     std::unique_ptr<RStretcher> stretcher;
 };
--- src/ice/core/effect/PitchAlter.cpp
+++ src/ice/core/effect/PitchAlter.cpp
@@ -1,6 +1,5 @@
 #include <rubberband/RubberBandStretcher.h>
 
-#include <cmath>
 #include <ice/core/effect/PitchAlter.hpp>
 
 namespace ice {
@@ -10,10 +9,9 @@ void PitchAlter::apply_effect(AudioBuffer& output, const AudioBuffer& input) {
     if (!stretcher) {
         stretcher = std::make_unique<RStretcher>(input.afmt);
     }
-    // 设置 RubberBand 的参数
-    // 音高倍率 = 2 ^ (半音数 / 12)
-    const double pitch_ratio = std::pow(2.0, pitch_semitones.load() / 12.0);
-    stretcher->set_pitch_ratio(pitch_ratio);
+
+    stretcher->set_pitch_ratio(pitch_scale.load());
+
     // 实时处理
     stretcher->process(output, input);
 }
